822dda9632dbcb1983aa0e5b243ce354df32ee8b
John Murray <5672686+JRMurr@users.noreply.github.com>
2021-11-27 19:33:56 -0500

    2019 day 1

--- haskell/app/Day01/Mod.hs
+++ haskell/app/Day01/Mod.hs
@@ -3,16 +3,26 @@ module Day01.Mod where
 import PseudoMacros (__FILE__)
 import Utils.Mod (getInputFile, readInput)
 
+getFuel :: Integer -> Integer
+getFuel mass = (mass `div` 3) - 2
+
+getFuelRec :: Integer -> Integer
+getFuelRec mass
+  | needed >= 9 = needed + getFuelRec needed
+  | otherwise = needed
+  where
+    needed = getFuel mass
+
 part1 :: IO ()
 part1 = do
   input <- readInput $ getInputFile $__FILE__
-  putStrLn "part1"
+  print $ sum (map (getFuel . read) input)
   return ()
 
 part2 :: IO ()
 part2 = do
   input <- readInput $ getInputFile $__FILE__
-  putStrLn "part2"
+  print $ sum (map (getFuelRec . read) input)
   return ()
 
 dispatch :: [(Int, IO ())]
--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,4 +1,17 @@
 module Main where
 
+-- need to update other-modules to add sub modules
+import qualified Day01.Mod as Day01 (dispatch)
+import System.Environment (getArgs)
+
+type DayDispatcher = [(Int, IO ())]
+
+dayMap :: [(Int, DayDispatcher)]
+dayMap = [(1, Day01.dispatch)]
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  [dayNumber, part] <- getArgs
+  let (Just dayDispatcher) = lookup (read dayNumber) dayMap
+  let (Just func) = lookup (read part) dayDispatcher
+  func
