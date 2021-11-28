0011b1f17dd182a9c4556f5f5be81aef57a6f76b
John Murray <5672686+JRMurr@users.noreply.github.com>
2021-11-27 20:28:35 -0500

    add script to add day

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -2,12 +2,19 @@ module Main where
 
 -- need to update other-modules to add sub modules
 import qualified Day01.Mod as Day01 (dispatch)
+import qualified Day02.Mod as Day02 (dispatch)
+-- Add day import
+
 import System.Environment (getArgs)
 
 type DayDispatcher = [(Int, IO ())]
 
 dayMap :: [(Int, DayDispatcher)]
-dayMap = [(1, Day01.dispatch)]
+dayMap =
+  [ (1, Day01.dispatch),
+    (2, Day02.dispatch)
+    -- Add day dispatch
+  ]
 
 main :: IO ()
 main = do
---
+++ haskell/setupDay.fish
@@ -0,0 +1,27 @@
+#!/usr/bin/env fish
+argparse --name=setupDay 'd/day=' -- $argv
+or exit
+
+set -l day $_flag_day
+
+if not string match --quiet --regex '^\d{2,}$' $day
+    echo $day must be a 2 digit number
+    exit 1
+end
+
+set -l dayStr Day$day
+
+# easy way to remove starting 0 if needed
+set -l dayNum (math $day)
+
+cp -r ./_dayTemplate ./app/$dayStr
+
+sed -i "s/DayXX/$dayStr/" ./app/$dayStr/Mod.hs
+
+sed -i "s/other-modules:/other-modules:\n        $dayStr.Mod/" ./aoc2019.cabal
+
+sed -i "s/-- Add day import/import qualified $dayStr.Mod as $dayStr (dispatch)\n-- Add day import\n/" ./app/Main.hs
+
+sed -i "s/-- Add day dispatch/,($dayNum, $dayStr.dispatch)\n    -- Add day dispatch\n/" ./app/Main.hs
+
+echo format Main.hs
